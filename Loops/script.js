50a33fffff20745ba0759d5343af1e1ca8b5c6d8
AshishKukreti2003 <ashishkukreti4546@gmail.com>
2025-08-19 23:11:33 +0530

    Loops Q&A

--- Loops/script.js
+++ Loops/script.js
@@ -1,2 +1,73 @@
 // for while do-while forin forof foreach
 
+
+// for(let i = 0   ; i<=100; i++){
+//     console.log(i);
+// }
+
+
+// let i= 0;
+// while(i<30){
+//     console.log(i);
+//     i++;
+// }
+
+
+// let i = 26;
+// do{
+//     console.log(i);
+//     i--;
+// }while(i>=24);
+
+
+// for(let i=4;i<=40;i++){
+//     if(i === 30)
+//         break;
+//     console.log(i);
+// }
+
+// for(let i =0;i<100;i++){
+//     if(i %10 === 0){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// for(let i=1;i<=20;i++){
+//     if(i%2 === 0){
+//         console.log(i);
+//     }
+// }
+
+// for(let i = 1;i<= 20;i++){
+//     if(i%2 === 1){
+//         console.log(i);
+//     }
+// }
+
+// let val = prompt("Enter a number: ");
+// for(let i = 1;i<=val;i++){
+//     if(i%2 === 0){
+//         console.log(`${i} is even`);
+//     }else{
+//         console.log(`${i} is odd`);
+//     }
+// }
+
+// let count = 0;
+// for(let i = 1;i<101;i++){
+//     if(i%3 === 0 && i%5 === 0){
+//         console.log(i);
+//         count++;
+//     }
+// }
+// console.log(count);
+
+
+// for(let i = 1;i<100;i++){
+//     console.log(i);
+//     if(i%7 === 0){
+//         break;
+//     }
+// }
+
