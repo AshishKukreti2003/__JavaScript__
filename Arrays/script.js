9e7b8533790c2507722c58412368c64ea5cd941a
AshishKukreti2003 <ashishkukreti4546@gmail.com>
2025-08-23 22:48:35 +0530

    Arrays Practice

--- Arrays/script.js
+++ Arrays/script.js
@@ -53,9 +53,69 @@ let newTestArr = testArr.map(function(val){
 })
 console.log(newTestArr);
 
+// jab bhi aapko aisa koi case dikh jaaye jaha par ek array se naya array banega and wo naya array kuch values ko rakhega tab map lagega
+
 let newTestArr1 = testArr.map(function(val){
     if(val %2 == 0){
         return val;
     }
 });
 console.log(newTestArr1);
+
+// filter
+let array1 = [1,2,3,4,5,6,7,8,9];
+let newArray = array1.filter(function(val){
+    if(val %3 == 0){
+        return true;
+    }
+});
+console.log(newArray);
+
+// reduce
+let array2 = [1,2,3,4,5,6];
+let ans = array2.reduce(function(accumulator, val){
+    return accumulator + val;
+},0); // 0 to set accumulator value initially
+console.log(ans);
+
+// find
+let array3 = [
+    {id: 1, key: 1},
+    {id:2, key: 2},
+    {id:3, key: 1}
+];
+let newArray3 = array3.find(function(val){
+    return val.key == 1;
+});
+
+console.log(newArray3); // returns first match only
+
+// some
+let array4 = [67,89,45,67,34];
+let newArray4 = array4.some(function(val){
+    return val > 50;
+});
+console.log(newArray4); // it will check only one condition is true
+
+// every
+let array5 = [1,2,3,4,5,7,9];
+let newArray5 = array5.every(function(val){
+    return val %2 == 0;
+});
+console.log(newArray5); // it will check all conditions are true or not 
+
+// destructing 
+let desArr = [1,2,3,4,5,6];
+// let [a,b] = desArr
+// console.log(a,b); // 1  2
+let [a,b,,c] = desArr;
+console.log(a,b,c); // 1  2  4
+
+// spread operator
+let arr8 = [1,2,3,4,5,6,7,8,9];
+let new_Arr = arr8; // it is a reference copy of arr8 if we change new_Arr will also change in arr8
+let new_Arr1 = [...arr8]; // it will create a new copy of arr8
+
+//6:56
+
+
